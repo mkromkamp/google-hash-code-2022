35200dae8b04459260a151d8470152e1fa94d141
James Lappin <james.lappin@checkout.com>
2022-02-24 18:04:15 +0000

    Adding classes for contri

--- src/HashCode/Parsers/Input.cs
+++ src/HashCode/Parsers/Input.cs
@@ -2,13 +2,36 @@ namespace HashCode.Parsers;
 
 public class Input
 {
+    private int NumOfContributors;
+    private int NumOfProjects;
+
     public Challenge Parse(string file)
     {
         // TODO; Parse input file and return Challenge
+
+        var fileLines = File.ReadLines(file).ToArray();
+        int currentLine = 0;
+
+        var firstLine = fileLines[0];
+        firstLine.Split(' ').ToList();
+        NumOfContributors = firstLine[0];
+        NumOfProjects = firstLine[1];
         
+
+
         return new();
     }
-    
-    
-    
+
+
+    public class Skill
+    {
+        public string Name { get; set; }
+        public string Level { get; set; }
+    }
+
+    public class Contributor
+    {
+        public string Name { get; set; }
+        public List<Skill> Skills { get; set; }
+    }
 }
