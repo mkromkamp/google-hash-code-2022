d4eef56989cf83169f59c2442751bbce118b6563
Martin Kromkamp <m.kromkamp@gmail.com>
2022-02-24 21:39:40 +0000

    final bits

--- src/HashCode/Solution.Martin.cs
+++ src/HashCode/Solution.Martin.cs
@@ -7,41 +7,92 @@ public partial class Solution
     public Solution SolveMartin(Challenge challenge)
     {
         var projects = new Dictionary<string, List<string>>();
-        
-        foreach (var project in challenge.Projects)
+
+        foreach (var project in challenge.Projects.OrderBy(p => p.DaysBeforeBestBefore))
         {
-            var contributors = new List<string>();
-            
-            // Try to find a mentor, if not available ignore the project
-            if (!TryGetMentor(project.RequiredSkills, challenge.Contributors, out var mentor))
+            // Try to fill all required skills, if not possible skip project
+            if (!TryGetContributors(project.RequiredSkills, challenge.Contributors, out var contributors)
+                || contributors.Count < project.RequiredSkills.Count)
                 continue;
+
+            // // Try to find a mentor, if not available ignore the project
+            // if (!TryGetMentor(project.RequiredSkills, challenge.Contributors, out var mentor))
+            //     continue;
+            //
+            // // Add contributor
+            // contributors.Add(mentor.Item1.Name);
+            //
+            // // If we need more than one skill we need to find for mentee
+            // if (project.RequiredSkills.Count > 1)
+            // {
+            //     // Don´t look for the skill of the mentor
+            //     var remainingSkills = project.RequiredSkills.Where(s => !s.Name.Equals(mentor.Item2.Name)).ToList();
+            //     var remainingContributor = challenge.Contributors.Where(s => !s.Name.Equals(mentor.Item1.Name)).ToList();
+            //
+            //     // Look to fill remaining roles with mentee
+            //     // If we don´t find a mentee for all remaining roles skip the project
+            //     if (!GetMentee(remainingSkills, remainingContributor, out var mentee)
+            //         || mentee.Count != project.RequiredSkills.Count-1)
+            //         continue;
+            //
+            //     contributors.AddRange(mentee.Select(m => m.Name));
+            // }
             
-            // Add contributor
-            contributors.Add(mentor.Item1.Name);
-
-            // If we need more than one skill we need to find for mentee
-            if (project.RequiredSkills.Count > 1)
-            {
-                // Don´t look for the skill of the mentor
-                var remainingSkills = project.RequiredSkills.Where(s => !s.Name.Equals(mentor.Item2.Name)).ToList();
-                var remainingContributor = challenge.Contributors.Where(s => !s.Name.Equals(mentor.Item1.Name)).ToList();
-
-                // Look to fill remaining roles with mentee
-                // If we don´t find a mentee for all remaining roles skip the project
-                if (!GetMentee(remainingSkills, remainingContributor, out var mentee)
-                    || mentee.Count != project.RequiredSkills.Count-1)
-                    continue;
-
-                contributors.AddRange(mentee.Select(m => m.Name));
-            }
-            
-            projects.Add(project.Name, contributors);
+            projects.Add(project.Name, contributors.Select(c => c.Name).ToList());
         }
 
         return new Solution
         {
             Projects = projects
         };
+    }
+    
+    private bool TryGetContributors(List<Input.Skill> skills, List<Input.Contributor> contributors, out List<Input.Contributor> result)
+    {
+        result = new();
+
+        var skillNames = skills.Select(s => s.Name).ToList();
+        // Order potential contributor by number of matching skills
+        var ordered = contributors
+            .OrderBy(c =>
+            {
+                // Matching skill count
+                return c.Skills.Count(s => skillNames.Contains(s.Name));
+            })
+            .Select(c => c)
+            .ToList();
+
+        // Remaining skills
+        var remainingSkills = skills;
+        var remainingContributors = ordered;
+        while (remainingSkills.Any())
+        {
+            var foundMatch = false;
+            foreach (var contributor in remainingContributors)
+            {
+                var firstSkill = remainingSkills.First();
+                var matchingSkill = contributor.Skills.FirstOrDefault(s =>
+                    s.Name.Equals(firstSkill.Name) && s.Level >= firstSkill.Level);
+                
+                // Find if this contributor has the ability to be mentor for the first skill
+                if (matchingSkill is null)
+                    continue;
+                
+                remainingSkills.Remove(firstSkill);
+                remainingContributors.Remove(contributor);
+                result.Add(contributor); // Add or ignore if already added
+                foundMatch = true;
+                break;
+            }
+
+            if (!foundMatch)
+                break;
+        }
+
+        if (remainingSkills.Any())
+            result = new();
+
+        return result.Any();
     }
 
     private bool TryGetMentor(List<Input.Skill> skills, List<Input.Contributor> contributors, out (Input.Contributor?, Input.Skill) mentor)
