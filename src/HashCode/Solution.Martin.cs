ca227a9ad84d77c33267ad3ca47b420760806747
Martin Kromkamp <m.kromkamp@gmail.com>
2022-02-24 18:30:27 +0000

    Soltuion

--- src/HashCode/Solution.Martin.cs
+++ src/HashCode/Solution.Martin.cs
@@ -2,4 +2,8 @@ namespace HashCode;
 
 public partial class Solution
 {
+    public static Solution SolveMartin(Challenge challenge)
+    {
+        return new ();
+    }
 }
--- src/HashCode/Solution.cs
+++ src/HashCode/Solution.cs
@@ -2,6 +2,11 @@ namespace HashCode;
 
 public partial class Solution
 {
+    /// <summary>
+    /// Project name + Contributor names
+    /// </summary>
+    public Dictionary<string, List<string>> Projects { get; set; } = new();
+
     public virtual Solution SolveChallenge(Challenge challenge)
     {
         return new();
