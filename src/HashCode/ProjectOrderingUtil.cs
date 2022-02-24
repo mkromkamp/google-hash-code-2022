e591b1d200b1c4f12e3e0f595666c0135283ee79
Nick Saxelby <Nick.Saxelby@Checkout.com>
2022-02-24 18:42:43 +0000

    Added ordering by PPDPR

---
+++ src/HashCode/ProjectOrderingUtil.cs
@@ -0,0 +1,35 @@
+﻿using static HashCode.Parsers.Input;
+
+namespace HashCode
+{
+    public static class ProjectOrderingUtil
+    {
+        public static List<Project> OrderProjectByPPDPR(List<Project> inputProjects)
+        {
+            List<PPDPRProjectScoringObj> building = new List<PPDPRProjectScoringObj>();
+
+            foreach(var project in inputProjects)
+            {
+                building.Add(new PPDPRProjectScoringObj()
+                {
+                    Score = GetPPDPR(project),
+                    Project = project
+                });
+            }
+
+            return building.OrderByDescending(a => a.Score).Select(a => a.Project).ToList();
+
+        }
+
+        private static double GetPPDPR(Project p)
+        {
+            return (p.Points / p.DaysToComplete) /(double)p.RequiredSkills.Count();
+        }
+    }
+
+    public class PPDPRProjectScoringObj
+    {
+        public double Score { get; set; }
+        public Project Project { get; set; }
+    }
+}
--- test/HashCode.Tests/UnitTest1.cs
+++ test/HashCode.Tests/UnitTest1.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using Xunit;
+using static HashCode.Parsers.Input;
 
 namespace HashCode.Tests;
 
@@ -8,5 +10,42 @@ public class UnitTest1
     public void Test1()
     {
 
+    }
+
+    [Fact]
+    public void TestOrderingPPDPRWorks1()
+    {
+        List<Project> inputs = new List<Project>();
+        inputs.Add(new Project()
+        {
+            Name = "1",
+            DaysToComplete = 10,
+            Points = 50,
+            RequiredSkills = new List<Skill>()
+            {
+                new Skill()
+                {
+
+                },
+                new Skill()
+                {
+
+                }
+            }
+        });
+        inputs.Add(new Project()
+        {
+            Name="2",
+            DaysToComplete = 5,
+            Points = 15,
+            DaysBeforeBestBefore = 6,
+            RequiredSkills = new List<Skill>()
+            {
+                new Skill()
+                { }
+            }
+        });
+        var result = ProjectOrderingUtil.OrderProjectByPPDPR(inputs);
+        Assert.Equal("2", result[0].Name);
     }
 }
