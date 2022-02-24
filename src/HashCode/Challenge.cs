716ded9aac4caa2fa10334a8f8e25185b9577039
Martin Kromkamp <m.kromkamp@gmail.com>
2022-02-24 18:21:15 +0000

    Input and challenge

--- src/HashCode/Challenge.cs
+++ src/HashCode/Challenge.cs
@@ -1,6 +1,10 @@
+using HashCode.Parsers;
+
 namespace HashCode;
 
 public partial class Challenge
 {
+    public List<Input.Project> Projects { get; set; }
 
+    public List<Input.Contributor> Contributors { get; set; }
 }
